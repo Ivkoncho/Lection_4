2178e504ba95594efb8c298b8eb695ec6440c095
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-03-08 20:06:09 +0200

    Length charAT

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        String a = new String("Ivaylo");
+
+        System.out.println("The length is " + a.length());
+        for (int i =0;i<a.length(); i++)
+        {
+            System.out.println(a.charAt(i));
+
+        }
     }
+
+
 }
